28337417f3f3d27a8a7b22d2b94ca8d9c9c0914b
famemost <95706039+famemost@users.noreply.github.com>
2023-09-29 16:01:45 +0800

    Update Main.java
    远程仓库文件被修改

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("First commit!");
         System.out.println("Second commit!");
+        System.out.println("远程仓库发生修改");
     }
 }
